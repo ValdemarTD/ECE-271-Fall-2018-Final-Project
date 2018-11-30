6411138bdca2c9a33119d7e9a3b1b8c690ba1323
Zachary Lee <zacharyianlee@gmail.com>
2018-11-29 22:18:31 -0800

    Finished color parser and support for multiple color modes
    Can do 64 color or 4096 color. Switched by button press.

--- ColorParser.sv
+++ ColorParser.sv
@@ -1,25 +1,64 @@
 module ColorParser(
-  input logic [5:0] color_code,
-  output logic [1:0] Red,
-  output logic [1:0] Blue,
-  output logic [1:0] Green
+  input logic [11:0] color_code,
+  input logic [0:0] ModeSwitch,
+  output logic [3:0] Red,
+  output logic [3:0] Blue,
+  output logic [3:0] Green
+
   );
 
-  reg [5:0] num;
-  reg [5:0] num2;
-  reg [5:0] num3;
-  reg [5:0] num4;
+  reg [11:0] num;
+  reg [11:0] num2;
+  reg [11:0] num3;
+  reg [11:0] num4;
+  reg [0:0] Mode;
+
+  initial begin
+    Mode = 0;
+  end
+
+  always_ff @(posedge ModeSwitch)
+    begin
+      case(Mode)
+      0 : Mode = 1;
+      1 : Mode = 0;
+      default : Mode = 0;
+    endcase
+  end
+
 
   always_ff @(*)
   begin
-    num <= color_code;
+  case(Mode)
+    0 : begin
+        num <= color_code;
+        num2 <= num  % 64;
+        num3 <= num2 % 16;
+        num4 <= num3 % 4;
+        Red <= num4;
+        Blue <= (num3 - num4)/4;
+        Green <= (num2 - num3)/16;
+      end
 
-    num2 <= num  % 64;
-    num3 <= num2 % 16;
-    num4 <= num3 % 4;
-    Red <= num4;
-    Blue <= (num3 - num4)/4;
-    Green <= (num2 - num3)/16;
+    1 : begin
+        num <= color_code;
+        num2 <= num % 4096;
+        num3 <= num2 % 256;
+        num4 <= num3 % 16;
+        Red <= num4;
+        Blue <= (num3 - num4)/16;
+        Green <= (num2 - num3)/256;
+      end
 
+    default : begin
+        num <= color_code;
+        num2 <= num  % 64;
+        num3 <= num2 % 16;
+        num4 <= num3 % 4;
+        Red <= num4;
+        Blue <= (num3 - num4)/4;
+        Green <= (num2 - num3)/16;
+      end
+    endcase
   end
 endmodule
---
+++ VGA_Interface.sv
@@ -0,0 +1,36 @@
+module VGA_Interface(
+  input logic [0:0] Clock, ModeSwitch,
+  input logic [11:0] Color,
+  output logic [3:0] Red, Green, Blue
+);
+
+
+  reg [3:0] R, G, B;
+
+    ColorParser parse(
+      .color_code(Color),
+      .ModeSwitch(ModeSwitch),
+      .Red(R),
+      .Green(G),
+      .Blue(B)
+    );
+
+    ColorDecoder RDec(
+      .Color(R),
+      .ModeSwitch(ModeSwitch),
+      .Pins(Red)
+    );
+
+    ColorDecoder GDec(
+      .Color(G),
+      .ModeSwitch(ModeSwitch),
+      .Pins(Green)
+    );
+
+    ColorDecoder BDec(
+      .Color(B),
+      .ModeSwitch(ModeSwitch),
+      .Pins(Blue)
+    );
+
+endmodule
